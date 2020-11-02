1b709b146315bcf093e1b4a33cf4a31d54e7e7be
hamed-fs <hamed@firstsource.io>
2020-11-02 10:15:53 +0330

    add basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(App());
@@ -26,13 +27,70 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Widget child = DottedBorder(
+    color: Colors.black,
+    strokeWidth: 1,
+    borderType: BorderType.RRect,
+    radius: Radius.circular(8),
+    child: SampleShape(color: Colors.transparent),
+  );
+
+  Widget placeHolder = SampleShape(color: Colors.grey);
+
+  Widget dataWidget = SampleShape(color: Colors.green.withOpacity(0.5));
+
+  Widget baseWidget = SampleShape(color: Colors.green);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Draggable<Widget>(
+              data: dataWidget,
+              child: baseWidget,
+              childWhenDragging: placeHolder,
+              feedback: dataWidget,
+            ),
+            DragTarget<Widget>(
+              onWillAccept: (Widget data) => true,
+              onAccept: (data) {
+                child = baseWidget;
+                setState(() {});
+              },
+              builder: (context, candidateData, rejectedData) => child,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SampleShape extends StatelessWidget {
+  const SampleShape({
+    Key key,
+    this.boxSize = 128,
+    this.color = Colors.grey,
+  }) : super(key: key);
+
+  final double boxSize;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: boxSize,
+      height: boxSize,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(8),
+      ),
     );
   }
 }
